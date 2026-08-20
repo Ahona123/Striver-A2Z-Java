2207f39e98db28e70da0a7ded7cfb4c9df46d4c8
AHONA GHOSH <161322857+Ahona123@users.noreply.github.com>
2026-08-20 10:24:50 +0530

    Added Java pattern solutions

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-public class Patterns {
-    public static void main(String[] args){
+public class Pattern1{
+    public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i =0;i<n;i++){
@@ -8,6 +8,6 @@ public class Patterns {
                 System.out.print("*");
             }
             System.out.println();
-            }
         }
     }
+}
