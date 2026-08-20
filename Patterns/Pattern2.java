dc5d6939a7e7236674af370a593755cd0d0b197d
AHONA GHOSH <ahonaghosh@Mac.Dlink>
2026-08-20 10:24:50 +0530

    Added Java pattern solutions

--- Patterns/Pattern2.java
+++ Patterns/Pattern2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-public class Patterns2 {
-    public static void main(String[] args){
+public class Pattern2{
+    public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i =0;i<n;i++){
--- Patterns/Pattern3.java
+++ Patterns/Pattern3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-public class Pattern3 {
-    public static void main(String[] args){
+public class Pattern3{
+    public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i =1;i<=n;i++){
