62d18aaec6262b517b2991005026511804c1ea8d
AHONA GHOSH <161322857+Ahona123@users.noreply.github.com>
2026-09-14 09:24:15 +0530

    Added Sorted Rotated array

---
+++ Arrays/SortedRotated.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class SortedRotated {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>arr[(i+1) % n]){
+                count++;
+            }
+        }
+        if(count<=1){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+     }
+    }
